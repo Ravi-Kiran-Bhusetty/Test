e5c36670765775c03cb231e902c7acb88233ade8
unknown <ravikiranb.2205@gmail.com>
2019-02-23 10:31:15 +0530

    Final

--- src/CatsMouse.java
+++ src/CatsMouse.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class CatsMouse {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		int catA = sc.nextInt();
 		int catB = sc.nextInt();
 		int mouseC = sc.nextInt();
-		int a = Math.abs(mouseC-catA);
-		int b = Math.abs(mouseC-catB);
-		if(a<b)
+		int a = Math.abs(mouseC - catA);
+		int b = Math.abs(mouseC - catB);
+		if (a < b)
 			System.out.println("Cat A");
-		else if(a>b)
+		else if (a > b)
 			System.out.println("Cat B");
 		else
 			System.out.println("Mouse C");
--- src/MarsExploration.java
+++ src/MarsExploration.java
@@ -3,36 +3,32 @@ import java.util.Scanner;
 public class MarsExploration {
 
 	public static void main(String[] args) {
-	
-		Scanner sc= new Scanner(System.in);
-		System.out.println("Enter the string");
+
+		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
 		char ch[] = s.toCharArray();
 		int count = 0;
-		//if(ch[0] != 'S')
-			//count++;
 		for (int i = 1; i < ch.length; i++) {
-			
-			if(ch[i] == 'O') {
-				
-				if(ch[i-1] != 'S'){
-					count++;	
+
+			if (ch[i] == 'O') {
+
+				if (ch[i - 1] != 'S') {
+					count++;
 				}
-				if(ch[i+1] != 'S'){
-					count++;	
+				if (ch[i + 1] != 'S') {
+					count++;
 				}
-				i+=2;
-			}
-			else {
+				i += 2;
+			} else {
 				count++;
-				if(ch[i-1] != 'S'){
-					count++;	
+				if (ch[i - 1] != 'S') {
+					count++;
 				}
-				if(ch[i+1] != 'S'){
-					count++;	
+				if (ch[i + 1] != 'S') {
+					count++;
 				}
-				i+=2;
-			}	
+				i += 2;
+			}
 		}
 		System.out.println(count);
 
